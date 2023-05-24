ffb9efa3f4e014f7ee75daf67a35a5cffe1be9fa
AlmightyDuckman <115139566+MariusTusvik@users.noreply.github.com>
2023-05-24 17:56:51 +0200

    moved home-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
